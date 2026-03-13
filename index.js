0bcd8affdbc92e09bd7b4718d349082f951d23fc
That1Planet <138253520+That1Planet@users.noreply.github.com>
2026-03-14 11:23:07 +1300

    Changed style.top to style.visiblity

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ let lastScrollY = window.scrollY;
 window.addEventListener("scroll", () => {
   console.log("checking")
   if (lastScrollY < window.scrollY) {
-    // Scrolling Down - hide the nav
-    nav.style.top = "-1000px";
+    // Scrolling Down - hide the 
     //classList.add("navbar--hidden");
+    nav.style.visibility = "hidden";
     console.log("adding class")
     console.log(nav.style.top)
   } else {
     // Scrolling Up - show the nav
-    nav.style.top = "0px";
+    //nav.style.top = "0px";
+    nav.style.visibility = "visible";
     console.log("removing class")
     console.log(nav.style.top)
   }
