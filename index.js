6de7ca41e0bd5d9321ab510b1f1912f149c5f55b
damm-its-a-plantery-git-hub <teaa@newlands.school.nz>
2026-03-16 14:32:37 +1300

    nav bar works properly now

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-//const nav = document.querySelector(".navbar");
 const nav = document.getElementById("navbar");
 let lastScrollY = window.scrollY;
 
 window.addEventListener("scroll", () => {
   console.log("checking")
   if (lastScrollY < window.scrollY) {
-    // Scrolling Down - hide the 
-    //classList.add("navbar--hidden");
-    nav.style.visibility = "hidden";
-    console.log("adding class")
-    console.log(nav.style.top)
+    // Scrolling Down - hide the navbar
+    //nav.style.visibility = "hidden";
+    nav.style.top = "-200px";
   } else {
     // Scrolling Up - show the nav
-    //nav.style.top = "0px";
-    nav.style.visibility = "visible";
-    console.log("removing class")
-    console.log(nav.style.top)
+    //nav.style.visibility = "visible";
+    nav.style.top = "10px";
   }
 
   // Update the position for the next scroll event
