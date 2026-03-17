049369aaa6b015b6097c5c02a1835d0d141c6e08
damm-its-a-plantery-git-hub <teaa@newlands.school.nz>
2026-03-18 09:12:28 +1300

    gave up on makeing the buffer, it's fine just using plain sticky

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const nav = document.getElementById("navbar");
 let lastScrollY = window.scrollY;
-const buffer = 10
+const top_buffer = 125
 window.addEventListener("scroll", () => {
-  console.log("checking",lastScrollY," ",window,scrollY)
-  var diffrence = Math.abs(lastScrollY - window.scrollY)
-  if (diffrence > buffer){
-    if (lastScrollY < window.scrollY) {
-      // Scrolling Down - hide the navbar
-      nav.classList.add('navbar--hidden');
-    } else {
-      // Scrolling Up - show the nav
-      nav.classList.remove('navbar--hidden')
-    }
+  //if (window.scrollY < top_buffer) 
+  //  {nav.style.position = "static"}
+  //else {nav.style.position = "sticky"}
+    
+  if (lastScrollY < window.scrollY ) {
+    // Scrolling Down - hide the navbar
+    nav.classList.add('navbar--hidden');
+  } else {
+    // Scrolling Up - show the nav
+    nav.classList.remove('navbar--hidden')
   }
   // Update the position for the next scroll event
   lastScrollY = window.scrollY;
