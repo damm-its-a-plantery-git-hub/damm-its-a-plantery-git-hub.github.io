6a9877dbaefca29b0ba77670491ea78cd041a350
damm-its-a-plantery-git-hub <teaa@newlands.school.nz>
2026-03-18 08:53:10 +1300

    does a buffer work

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const nav = document.getElementById("navbar");
 let lastScrollY = window.scrollY;
-
+const buffer = 10
 window.addEventListener("scroll", () => {
-  console.log("checking",lastScrollY,window,scrollY)
-  if (lastScrollY < window.scrollY) {
-    // Scrolling Down - hide the navbar
-    //nav.style.top = "-200px";
-    nav.classList.add('navbar--hidden');
-  } else {
-    // Scrolling Up - show the nav
-    //nav.style.top = "10px";
-    nav.classList.remove('navbar--hidden')
+  console.log("checking",lastScrollY," ",window,scrollY)
+  var diffrence = Math.abs(lastScrollY - window.scrollY)
+  if (diffrence > buffer){
+    if (lastScrollY < window.scrollY) {
+      // Scrolling Down - hide the navbar
+      nav.classList.add('navbar--hidden');
+    } else {
+      // Scrolling Up - show the nav
+      nav.classList.remove('navbar--hidden')
+    }
   }
-
   // Update the position for the next scroll event
   lastScrollY = window.scrollY;
 });
