075183df0ac209ab81ac5dda728dc1f5a5497293
damm-its-a-plantery-git-hub <teaa@newlands.school.nz>
2026-03-18 09:25:28 +1300

    started implamenting search

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const nav = document.getElementById("navbar");
 let lastScrollY = window.scrollY;
-const top_buffer = 125
+
 window.addEventListener("scroll", () => {
-  //if (window.scrollY < top_buffer) 
-  //  {nav.style.position = "static"}
-  //else {nav.style.position = "sticky"}
-    
   if (lastScrollY < window.scrollY ) {
     // Scrolling Down - hide the navbar
     nav.classList.add('navbar--hidden');
