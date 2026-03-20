24ebd9c2df3a233a0a30f6b00caa206e5be35097
damm-its-a-plantery-git-hub <teaa@newlands.school.nz>
2026-03-20 13:02:12 +1300

    ahhh

---
+++ content_ver1.js
@@ -0,0 +1,5 @@
+Content = {
+    "Heading":"",
+    "Picture":"",
+    "Discriptor":""
+}
--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const nav = document.getElementById("navbar");
 let lastScrollY = window.scrollY;
 
+async function set_navbar() {
+  //let navbar = fetch("navbar.html").then(Response.text(nav.innerHTML = navbar))
+  //fetch("navbar.html")
+   // .then(Response => Response.text()).then(myconvertedtext => console.log(myconvertedtext))
+  
+  //console.log("Putting html in",fetch("navbar.html"), "vs the actal code", nav.innerHTML)
+  const url = "navbar.json"
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+  const result = await response.json();
+  console.log(result);
+  nav.innerHTML = result.content
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+
 window.addEventListener("scroll", () => {
   if (lastScrollY < window.scrollY ) {
     // Scrolling Down - hide the navbar
