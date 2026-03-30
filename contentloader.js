d6bc9078b5c163890d4bd457c69c685b20b00560
That1Planet <andrewtea100@gmail.com>
2026-03-30 15:05:47 +1300

    IT WORKS

--- contentloader.js
+++ contentloader.js
@@ -1,24 +1,49 @@
 async function load_content(type) { //Sets up the content page
     const indexurl = "contentindex.json"
     
+    let best
+
     try {
     const response = await fetch(indexurl);
     if (!response.ok) {
         throw new Error(`Response status: ${response.status}`);
     }
     const result = await response.json();
-    let content = JSON.parse(result); 
-    console.log (content)
-    //for (let i = 0; i < entries; i++) {
-      //  let content =  {
-        //    "fetured" : 10,
-          //  "internal_name" : "001",
-            //"displayname" : "headline"
-     //   }
+    //console.log(result)
+    let entries = result.length;
+    let minfeat = -1
+    Object.values(result).forEach (value => {
+        //console.log(value);
+        //console.log(value.fetured)
+        if (value.fetured > minfeat) {
+            minfeat = value.fetured
+            best = value
+        }
+    })
+    console.log(best)
 
-
-//    }
     } catch (error) {
         console.error(error.message)
     }
+
+    //Gettig the postst
+
+    try {
+    const response = await fetch("/headlines/" + best.internal_name + ".md");
+    if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+    }
+    const result = await response.text();
+    console.log(result);
+    document.getElementById(type).innerHTML = document.getElementById(type).innerHTML + result
+    } catch (error) {
+    console.error(error.message);
+    }
+    //Uses try so it thorws errors properly 
+
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+    load_content("guides")
+});
+
