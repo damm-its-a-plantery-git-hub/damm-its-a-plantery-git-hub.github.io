5837b4acd2814f3d8b7ddda9a3740c9f17487e16
That1Planet <andrewtea100@gmail.com>
2026-04-20 15:06:49 +1200

    ahhh

--- index.js
+++ index.js
@@ -58,3 +58,63 @@ window.addEventListener("scroll", () => {
   lastScrollY = window.scrollY;
 });
 
+
+
+async function find_content(type,onetype) { //Sets up the content page
+    const indexurl = "contentindex" + type + ".json"
+    let max_headlines = 5
+    let current_headlines
+    try {
+    const response = await fetch(indexurl);
+    if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+    }
+    const result = await response.json();
+    let entries = result.length;
+    let highest_entry = -1
+    let best 
+    
+    
+    Object.values(result).forEach (value => {
+      Object.values(result).forEach (value => {
+        if (value.fetured > highest_entry && !value.applyed){
+          highest_entry = value.fetured
+          best = value
+        }
+      })
+      if (!best.applyed && current_headlines <= max_headlines ) {
+                best.applyed = true
+                current_headlines =+ 1
+                apply_content(best,type,onetype)
+            }
+    })
+    
+    Object.values(result).forEach (value => {
+            
+    })
+    } catch (error) {
+        console.error(error.message)
+    }  
+}
+
+async function apply_content(best,type,onetype) {
+    try {
+        const response = await fetch("/headlines/" + best.internalname + ".md");
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const result = await response.text();
+        console.log(result);
+        if (onetype == true){
+          document.getElementById("content").innerHTML = document.getElementById("content").innerHTML + result
+        } else {
+          document.getElementById(type).innerHTML = document.getElementById(type).innerHTML + result
+        }
+        
+    } catch (error) {
+        console.error(error.message);
+    }
+    //Uses try so it thorws errors properly 
+}
+
+
